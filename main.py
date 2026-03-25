f60e80460b068d64058b0834bb878bb5c262665a
Pentas <138345342+Pentees@users.noreply.github.com>
2026-03-26 02:30:01 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+
+import os
+from dotenv import load_dotenv
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from supabase import create_client, Client
+from fastapi.middleware.cors import CORSMiddleware
+
+# .envファイルのパスを指定して読み込む
+load_dotenv()
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
+supabase = create_client(url, key)
+
+class Item(BaseModel):
+    name: str
+    value: int
+
+@app.get("/")
+async def read_root():
+    return {"message_System": "System is Active Now!!"}
+
+@app.get("/Table/{table_name}")
+async def get_apiinfo(table_name : str):
+    response = supabase.table(table_name).select("*").match("Flag_Hidden", false).order('id', { ascending: false})
+    return response.data
+
+@app.get("/getTitle/")
+async def get_titles():
+    table_name = "Titles"
+    reffered_table_column = "Articles(id, title)"
+    response = supabase.table(table_name).select("*",reffered_table_column).eq("Flag_Hidden", "false").execute()
+    return response.data
