47316544e3eb908d1a833e2d0bc78fe694fdd76d
Pentas <138345342+Pentees@users.noreply.github.com>
2026-03-27 20:16:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,20 +21,17 @@ key: str =  os.environ.get("KEY")
 
 supabase = create_client(url, key)
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
-
-@app.get("/Table/{table_name}")
-async def get_apiinfo(table_name : str):
-    response = supabase.table(table_name).select("*").match("Flag_Hidden", false).order('id', { ascending: false})
-    return response.data
-
-@app.get("/getTitle/")
+@app.get("/supabaseAPI/getTitle/")
 async def get_titles():
     table_name = "Titles"
     reffered_table_column = "Articles(id, title)"
     response = supabase.table(table_name).select("*",reffered_table_column).eq("Flag_Hidden", "false").execute()
+    return response.data
+    
+@app.get("/supabaseAPI/getData/{id}")
+async def get_Data(id: str):
+    print(id)
+    response = supabase.table("Articles").select("*", "Titles(created_at, language)").eq("id", id).execute()
     return response.data
 
 def print_spec() -> None:
